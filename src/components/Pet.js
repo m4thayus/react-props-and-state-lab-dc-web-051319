ad625321bce0cb23a6a8440d2daefb3ff901db53
Matt Williams <matt.seongjun@gmail.com>
2019-07-17 12:30:58 -0400

    Stupid test still won't pass, // pending

--- src/components/Pet.js
+++ src/components/Pet.js
@@ -18,7 +18,7 @@ class Pet extends React.Component {
     // }
 
     render() {
-        return (
+        return (this.props.isAdopted ?
             <div className="card">
                 <div className="content">
                     <a className="header">
@@ -34,14 +34,29 @@ class Pet extends React.Component {
                     </div>
                 </div>
                 <div className="extra content">
-                    {/* {this.renderButton()} */}
-                    {
-                        this.props.isAdopted ? <button className="ui disabled button">Already adopted</button> : <button className="ui primary button" onClick={() => this.props.onAdoptPet(this.props.pet.id)}>Adopt pet</button>
-                    }
+                    <button className="ui disabled button">Already adopted</button> 
+                </div>
+            </div>
+            :
+            <div className="card">
+                <div className="content">
+                    <a className="header">
+                        {this.props.pet.gender === 'male' ? '♂' : '♀'} 
+                        {this.props.pet.name}
+                    </a>
+                    <div className="meta">
+                        <span className="date">{this.props.pet.type}</span>
+                    </div>
+                    <div className="description">
+                        <p>Age: {this.props.pet.age}</p>
+                        <p>Weight: {this.props.pet.weight}</p>
+                    </div>
+                </div>
+                <div className="extra content">
+                    <button className="ui primary button" onClick={() => this.props.onAdoptPet(this.props.pet.id)}>Adopt pet</button>
                 </div>
             </div>
         )
     }
 }
-
 export default Pet
