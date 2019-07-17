c583c2e9625de70b47064730bef5744a73203a98
Matt Williams <matt.seongjun@gmail.com>
2019-07-17 11:40:28 -0400

    Filter and render pet cards work

--- src/components/App.js
+++ src/components/App.js
@@ -4,36 +4,66 @@ import Filters from './Filters'
 import PetBrowser from './PetBrowser'
 
 class App extends React.Component {
-  constructor() {
-    super()
+    constructor() {
+        super()
 
-    this.state = {
-      pets: [],
-      filters: {
-        type: 'all'
-      }
+        this.state = {
+            pets: [],
+            filters: {
+                type: 'all'
+            }
+        }
     }
-  }
 
-  render() {
-    return (
-      <div className="ui container">
-        <header>
-          <h1 className="ui dividing header">React Animal Shelter</h1>
-        </header>
-        <div className="ui container">
-          <div className="ui grid">
-            <div className="four wide column">
-              <Filters />
+    onChangeType = (type) => {
+        this.setState({
+            filters: {
+                type: type
+            }
+        })
+    }
+
+    onFindPetsClick = () => {
+        let url 
+        if (this.state.filters.type === 'all') {
+            url = "/api/pets"
+        } else if (this.state.filters.type === 'cat') {
+            url = "/api/pets?type=cat"
+        } else if (this.state.filters.type === 'dog') {
+            url = "/api/pets?type=dog"
+        } else if (this.state.filters.type === 'micropig') {
+            url = "/api/pets?type=micropig"
+        } else {
+            return
+        }
+        fetch(url)
+            .then(response => response.json())
+            .then(petObjs => {
+                this.setState({
+                    pets: petObjs
+                })
+            });
+    }
+    
+    render() {
+        return (
+            <div className="ui container">
+                <header>
+                    <h1 className="ui dividing header">React Animal Shelter</h1>
+                </header>
+                <div className="ui container">
+                    <div className="ui grid">
+                        <div className="four wide column">
+                            <Filters onChangeType={this.onChangeType} onFindPetsClick={this.onFindPetsClick} />
+                        </div>
+                        <div className="twelve wide column">
+                            <PetBrowser pets={this.state.pets}/>
+                        </div>
+                    </div>
+                </div>
             </div>
-            <div className="twelve wide column">
-              <PetBrowser />
-            </div>
-          </div>
-        </div>
-      </div>
-    )
-  }
+        )
+    }
 }
 
 export default App
--- src/components/Filters.js
+++ src/components/Filters.js
@@ -1,25 +1,34 @@
 import React from 'react'
 
 class Filters extends React.Component {
-  render() {
-    return (
-      <div className="ui form">
-        <h3>Animal type</h3>
-        <div className="field">
-          <select name="type" id="type">
-            <option value="all">All</option>
-            <option value="cat">Cats</option>
-            <option value="dog">Dogs</option>
-            <option value="micropig">Micropigs</option>
-          </select>
-        </div>
 
-        <div className="field">
-          <button className="ui secondary button">Find pets</button>
-        </div>
-      </div>
-    )
-  }
+    handleChange = (event) => {
+        this.props.onChangeType(event.target.value)
+    }
+
+    handleClick = (event) => {
+        this.props.onFindPetsClick()
+    }
+
+    render() {
+        return (
+            <div className="ui form">
+                <h3>Animal type</h3>
+                <div className="field">
+                    <select name="type" id="type" onChange={this.handleChange}>
+                        <option value="all">All</option>
+                        <option value="cat">Cats</option>
+                        <option value="dog">Dogs</option>
+                        <option value="micropig">Micropigs</option>
+                    </select>
+                </div>
+
+                <div className="field">
+                    <button className="ui secondary button" onClick={this.handleClick}>Find pets</button>
+                </div>
+            </div>
+        )
+    }
 }
 
 export default Filters
